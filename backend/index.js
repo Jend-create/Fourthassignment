a6d39c0b19531068bedbe54645968644d1ff1ee0
Jend-create <jessdarkua1@gmail.com>
2020-12-16 08:16:11 +0000

    fourth work

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const mongoose = require('mongoose') 
+const config = require('./helpers/config')
+
+const authRoutes = require('./routes/auth')
+
+// Mongoose Connection
+
+const app = express()
+
+// Usage of middlewares
+app.use(express.json())
+
+app.use(authRoutes)
+
+
+app.listen(config.PORT, ()=> {
+    console.log(`Server started on PORT: ${config.PORT}`)
+})
